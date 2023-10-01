e72420a841a24c7e827daea330eecffb180d79af
pushkaraj23 <pushkaraj232002@gmail.com>
2023-10-01 15:24:01 +0530

    reviews

--- my-app/src/Review/Review.jsx
+++ my-app/src/Review/Review.jsx
@@ -1,7 +1,8 @@
-import React from "react"
-import ReviewModule from "./Review.module.css"
-import inverted from '../images/inverted-comma.png'
+import React from "react";
+import ReviewModule from "./Review.module.css";
+import inverted from "../images/inverted-comma.png";
 import Review_c from "../components/review_c";
+import User1 from "../images/therapist.png";
 
 export default function Review() {
   return (
@@ -9,14 +10,26 @@ export default function Review() {
       <div className={ReviewModule.first}>
         <img src={inverted} alt="" />
         <div className={ReviewModule.content}>
-            <Review_c />
+          <Review_c
+            review="I can actually see progress in my mental health which is something I haven’t been able to say in 15 years and its thanks to her."
+            userimg={User1}
+            username="Virginia Truglio"
+          />
+          <Review_c
+            review="I can actually see progress in my mental health which is something I haven’t been able to say in 15 years and its thanks to her."
+            userimg={User1}
+            username="Virginia Truglio"
+          />
         </div>
         <img src={inverted} alt="" />
       </div>
-      <div className={ReviewModule.toggle}>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className={ReviewModule.third}>
+        <div className={ReviewModule.toggle}>
+          <div id={ReviewModule.special}></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={ReviewModule.btn}>More Success Stories</div>
       </div>
     </div>
   );
